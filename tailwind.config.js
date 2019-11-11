80756d527d92955c6618c66e6480b7dd9b2c16ba
Scott Watermasysk <scottwater@gmail.com>
2019-11-11 10:51:24 -0500

    Enable typeface fonts and add Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: filteredFonts
+        sans: ["Inter", ...filteredFonts]
       },
       colors: {
         "polar-night": {
