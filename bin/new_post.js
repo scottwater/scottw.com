7ce2c58791646bedd984ae958934bbd054adcbf5
Scott Watermasysk <scottwater@gmail.com>
2019-11-19 06:04:13 -0500

    create empty posts

---
+++ bin/new_post.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+const argv = process.argv;
+const type = argv[2];
+const fileName = argv[3];
+
+if (type && fileName && ["post", "short", "link"].includes(type)) {
+  const frontMatter = `
+---
+title:
+date: ${new Date().toISOString()}
+${type === "link" ? "source_url:" : ""}
+`.trim();
+  const filePath = `site/${type}/${fileName}.md`;
+  fs.writeFile(filePath, `${frontMatter}\n---\n`, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      var spawn = require("child_process").spawn;
+      spawn("code", [filePath]);
+    }
+  });
+} else {
+  console.log(
+    "You must specify a type and file name. Type must be post, short, or link"
+  );
+}
