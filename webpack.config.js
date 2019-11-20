c80f93ad3bf096e75a42f9bb26e43dbd89f5d28b
Scott Watermasysk <scottwater@gmail.com>
2019-11-20 14:26:45 -0500

    remove unused html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = env => {
