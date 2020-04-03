e768c7869bf8a68f819b048007986af4e3895db0
Scott Watermasysk <scottwater@gmail.com>
2020-04-02 21:31:35 -0400

    tokyo 60

---
+++ bin/new_board.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+const argv = process.argv;
+const board = argv[2];
+
+const frontMatter = `
+---
+title:
+date: ${new Date().toISOString()}
+keywords: Mechanical Keyboard,
+image: /assets/images/gordons/${board}/1.jpeg
+`.trim();
+
+const jsonData = `{
+  "images": [
+    {
+      "img": "1.jpeg",
+      "alt": "The Board",
+      "desc": "Money shot"
+    }
+  ]
+}`;
+
+const jsonFilePath = `site/keeb/${board}.json`;
+fs.writeFile(jsonFilePath, `${jsonData}`, err => {
+  if (err) {
+    console.log(err);
+  }
+});
+
+const filePath = `site/keeb/${board}.md`;
+fs.writeFile(filePath, `${frontMatter}\n---\n`, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    var spawn = require("child_process").spawn;
+    spawn("code", [filePath]);
+  }
+});
