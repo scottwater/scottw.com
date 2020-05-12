358ceee227517fce4e20eeaf2a4ac70ae91eb755
Scott Watermasysk <scottwater@gmail.com>
2020-05-12 10:05:26 -0400

    minimize HTML

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,8 @@ const excerpts = require("./src/utils/excerpts");
 const markdown = require("./src/utils/markdown");
 const tweet = require("./src/utils/tweet");
 const { loadFilters } = require("./src/utils/filters");
-module.exports = function(eleventyConfig) {
+const htmlMinTransform = require("./src/utils/html-min-transform.js");
+module.exports = function (eleventyConfig) {
   // we need site/includes/packs.njk to be ignored in git
   // however, we still need it to watched for changes.
   // the .eleventyignore is used to tell Eleventy what to ignore
@@ -20,12 +21,10 @@ module.exports = function(eleventyConfig) {
   const options = {
     html: true,
     breaks: true,
-    linkify: true
+    linkify: true,
   };
 
-  const md = markdownIt(options)
-    .use(markdownItEmoji)
-    .use(markdownItFootnotes);
+  const md = markdownIt(options).use(markdownItEmoji).use(markdownItFootnotes);
 
   eleventyConfig.setLibrary("md", md);
 
@@ -48,6 +47,10 @@ module.exports = function(eleventyConfig) {
     return content;
   });
 
+  if (process.env.ELEVENTY_ENV === "production") {
+    eleventyConfig.addTransform("htmlmin", htmlMinTransform);
+  }
+
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.setDataDeepMerge(true);
@@ -56,23 +59,23 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
   eleventyConfig.addLayoutAlias("page", "layouts/page.njk");
 
-  eleventyConfig.addCollection("feed", collection => {
+  eleventyConfig.addCollection("feed", (collection) => {
     return collection
       .getFilteredByTag("blog")
       .reverse()
-      .filter(post => post.data.draft !== true)
+      .filter((post) => post.data.draft !== true)
       .slice(0, 20);
   });
 
-  eleventyConfig.addCollection("links", collection => {
+  eleventyConfig.addCollection("links", (collection) => {
     return collection.getFilteredByTag("link").reverse();
   });
 
-  eleventyConfig.addCollection("publishedPosts", collection => {
+  eleventyConfig.addCollection("publishedPosts", (collection) => {
     return collection
       .getFilteredByTag("blog")
       .reverse()
-      .filter(post => post.data.draft !== true);
+      .filter((post) => post.data.draft !== true);
   });
 
   // move to head so that it does not interfere
@@ -80,7 +83,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.setBrowserSyncConfig({
     // show 404s in dev. Borrowed from eleventy blog starter
     callbacks: {
-      ready: function(err, browserSync) {
+      ready: function (err, browserSync) {
         const content_404 = fs.readFileSync("dist/404.html");
 
         browserSync.addMiddleware("*", (_, res) => {
@@ -88,23 +91,23 @@ module.exports = function(eleventyConfig) {
           res.write(content_404);
           res.end();
         });
-      }
+      },
     },
     // scripts in body conflict with Turbolinks
     snippetOptions: {
       rule: {
         match: /<\/head>/i,
-        fn: function(snippet, match) {
+        fn: function (snippet, match) {
           return snippet + match;
-        }
-      }
-    }
+        },
+      },
+    },
   });
 
   return {
     dir: { input: "site", output: "dist", data: "_data", includes: "includes" },
     passthroughFileCopy: true,
     templateFormats: ["njk", "md", "css", "html", "yml"],
-    htmlTemplateEngine: "njk"
+    htmlTemplateEngine: "njk",
   };
 };
